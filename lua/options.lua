81fa9c655fb72e3a0eb9883e0f2229566c97ed2d
AMaugas <antoine.maugas@braindex.fr>
2024-02-27 17:06:31 +0100

    Adds my prefences for the options.
    Signed-off-by: AMaugas <antoine.maugas@braindex.fr>

--- lua/options.lua
+++ lua/options.lua
@@ -7,7 +7,7 @@
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -19,9 +19,7 @@ vim.opt.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
+vim.opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -60,5 +58,19 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- Configures NVim to use Delphi as default pascal language "flavor".
+vim.g.pascal_delphi = 1
+vim.g.pascal_one_line_string = 1
+vim.g.pascal_symbol_operator = 1
+
+-- Auto-reload buffer when files have been changed outside of NVim
+vim.o.autoread = true
+
+-- Modifies diagnostic appeareance to my liking
+vim.diagnostic.config {
+  severity_sort = true,
+  virtual_text = false,
+}
 
 -- vim: ts=2 sts=2 sw=2 et
