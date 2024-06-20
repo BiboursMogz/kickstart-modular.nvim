6d402aff818b243422b8207cbfd8b17610711420
AMaugas <antoine.maugas@braindex.fr>
2024-06-20 11:31:17 +0200

    Minor modifications to personal options.
    Signed-off-by: AMaugas <antoine.maugas@braindex.fr>

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,4 +1,11 @@
--- vim.cmd.language 'en_US'
+if vim.loop.os_uname().sysname == 'Windows_NT' then
+  vim.cmd.language 'en_US'
+end
 
+vim.opt.linebreak = true
+vim.o.linebreak = true
 vim.wo.linebreak = true
 vim.wo.breakindent = true
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
